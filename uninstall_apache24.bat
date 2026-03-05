53359ee9214c6718990e0dd6f554717ca61921c3
DioXine <dioxine@mail.ru>
2026-03-05 11:20:53 +1000

    Add files via upload

---
+++ uninstall_apache24.bat
@@ -0,0 +1,49 @@
+@echo off
+:: Переключаем консоль в UTF-8 для корректного отображения кириллицы
+chcp 65001 >nul
+setlocal
+
+:: Проверка прав администратора
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+    echo Requesting Admin Rights...
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:: --- НАСТРОЙКИ ПУТЕЙ ---
+set "LNK=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ApacheMonitor.lnk"
+set "BASE_PATH=C:\Program Files\Apache Software Foundation"
+
+echo [+] Шаг 1: Остановка и удаление службы Apache2.4...
+:: Останавливаем службу (флаг /y подтверждает остановку зависимых служб)
+net stop Apache2.4 /y >nul 2>&1
+sc delete Apache2.4 >nul 2>&1
+
+echo [+] Шаг 2: Завершение активных процессов...
+:: Убиваем монитор и сам httpd, чтобы разблокировать файлы в папке
+taskkill /f /im ApacheMonitor.exe >nul 2>&1
+taskkill /f /im httpd.exe >nul 2>&1
+
+echo [+] Шаг 3: Удаление ярлыка из автозагрузки...
+if exist "%LNK%" (
+    del /f /q "%LNK%"
+    echo [OK] Ярлык автозагрузки удален.
+)
+
+echo.
+echo ======================================================
+echo УДАЛЕНИЕ ЗАВЕРШЕНО УСПЕШНО!
+echo ======================================================
+echo Теперь вы можете вручную удалить системную папку:
+echo %BASE_PATH%
+echo ======================================================
+pause
